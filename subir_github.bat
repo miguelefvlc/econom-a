f31a8e09d0d06472ad3a5ccc41e71ad274b6114f
miguelef <mipasbuen.ef@gmail.com>
2026-07-22 16:23:00 +0200

    Actualizacion automatica 22/07/2026 16:22:59,76

--- subir_github.bat
+++ subir_github.bat
@@ -1,11 +1,11 @@
 @echo off
-cd /d %~dp0
+cd /d "%~dp0"
 echo Subiendo cambios a GitHub...
 git add -A
-git commit -m Actualizacion automatica %date% %time%
+git commit -m "Actualizacion automatica %date% %time%"
 git push
 echo.
 echo ========================================
-echo ¡Cambios subidos a GitHub correctamente!
+echo Cambios subidos a GitHub correctamente!
 echo ========================================
 pause
